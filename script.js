038a837bf80ebc618d9d9c1ed584df61bd741b7f
Lucas Eduardo <lucaseduardo.brum123@gmail.com>
2020-07-07 23:20:54 -0300

    Quase todos os layouts e Buttons prontos.

---
+++ script.js
@@ -0,0 +1,23 @@
+var respostasCertas = ['1', '3', '4', '2']
+var respostasUsuario = []
+function respondeu1(){
+    respostasUsuario.push('1')
+   
+}
+
+function respondeu2(){
+    respostasUsuario.push('2')
+
+}
+
+function respondeu3(){
+    respostasUsuario.push('3')
+
+}
+
+function respondeu4(){
+    respostasUsuario.push('4')
+    
+
+}
+
