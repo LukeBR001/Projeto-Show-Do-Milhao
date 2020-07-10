1711171217d49b9835e6a000e5f66aa88edc5b7c
Lucas Eduardo <lucaseduardo.brum123@gmail.com>
2020-07-09 23:34:35 -0300

    Adicionado a parte do JS e outros elementos HTML e CSS

---
+++ perguntas.js
@@ -0,0 +1,202 @@
+var questoes = [
+    {
+        pergunta: 'Qual é o signo do zodíaco de quem nasce no dia do ano-novo?',
+        respostas:{ 
+            a: 'Virgem',
+            b: 'Aquário',
+            c: 'Capricórnio',
+            d: 'Áries'
+        },
+        respostaCerta: 3
+    },
+    {
+        pergunta: 'Quem é a namorada do Mickey Mouse?',
+        respostas:{ 
+            a: 'Margarida',
+            b: 'Minnie',
+            c: 'A pequena sereia',
+            d: 'Olívia Palito'
+        },
+        respostaCerta: 2
+    },
+    {
+        pergunta: 'Em que estado brasileiro nasceu a apresentadora Xuxa?',
+        respostas:{
+            a: 'Rio de Janeiro',
+            b: 'Rio Grande do Sul',
+            c: 'Santa Catarina',
+            d: 'Goiás'
+        },
+        respostaCerta: 2
+    },
+    {
+        pergunta: 'A água ferve a quantos graus centígrados?',
+        respostas:{ 
+            a: '200',
+            b: '100',
+            c: '170',
+            d: '220'
+        },
+        respostaCerta: 2
+    },
+    {
+        pergunta: 'Qual lugar é também chamado de Santa Sé?',
+        respostas:{ 
+            a: 'Veneza',
+            b: 'Vitória',
+            c: 'Vancouver',
+            d: 'Vaticano'
+        },
+        respostaCerta: 4
+    },
+    {
+        pergunta: 'O dromedário tem quantas corcovas?',
+        respostas:{ 
+            a: 'Uma',
+            b: 'Duas',
+            c: 'Três',
+            d: 'Quatro'
+        },
+        respostaCerta: 1
+    },
+    {
+        pergunta: 'Um adulto sadio tem quantos dentes na boca?',
+        respostas:{ 
+            a: '18',
+            b: '24',
+            c: '32',
+            d: '36'
+        },
+        respostaCerta: 3
+    },
+    {
+        pergunta: 'Quantos jogadores um jogo de vôlei reúne na quadra?',
+        respostas:{ 
+            a: 'Seis',
+            b: 'Oito',
+            c: 'Dez',
+            d: 'Doze'
+        },
+        respostaCerta: 4
+    },
+    {
+        pergunta: 'Como é chamado quem nasce em Milão, na Espanha?',
+        respostas:{ 
+            a: 'Milanense',
+            b: 'Milanoso',
+            c: 'Milista',
+            d: 'Milanês'
+        },
+        respostaCerta: 4
+    },
+    {
+        pergunta: 'Sashimi é um prato originário de qual país?',
+        respostas:{ 
+            a: 'Japão',
+            b: 'China',
+            c: 'Índia',
+            d: 'Indonésia'
+        },
+        respostaCerta: 1
+    },
+    {
+        pergunta: 'Qual é o ponto mais alto da Terra?',
+        respostas:{ 
+            a: 'Everest',
+            b: 'Monte Sinai',
+            c: 'Monte Castelo',
+            d: 'Mont Blanc'
+        },
+        respostaCerta: 1
+    },
+    {
+        pergunta: 'Que rio corta a cidade de Londres, na Inglaterra?',
+        respostas:{ 
+            a: 'Tâmisa',
+            b: 'Sena',
+            c: 'Reno',
+            d: 'Aube'
+        },
+        respostaCerta: 1
+    },
+    {
+        pergunta: 'Qual é a moeda oficial da Alemanha?',
+        respostas:{ 
+            a: 'Lira',
+            b: 'Marco',
+            c: 'Franco',
+            d: 'Libra'
+        },
+        respostaCerta: 2
+    },
+    {
+        pergunta: 'O que os nilatelistas colecionam?',
+        respostas:{ 
+            a: 'Quadros',
+            b: 'Moedas',
+            c: 'Selos',
+            d: 'Figurinhas'
+        },
+        respostaCerta: 3
+    },
+    {
+        pergunta: 'A eletrônica é parte de qual ciência?',
+        respostas:{ 
+            a: 'Física',
+            b: 'Biológica',
+            c: 'Química',
+            d: 'Botânica'
+        },
+        respostaCerta: 1
+    },
+    {
+        pergunta: 'Que parte do corpo humano é infectada pela cólera?',
+        respostas:{ 
+            a: 'Garganta',
+            b: 'Intestino',
+            c: 'Pulmões',
+            d: 'Rins'
+        },
+        respostaCerta: 2
+    },
+    {
+        pergunta: 'Em que país você pode gastar rublos?',
+        respostas:{ 
+            a: 'Holanda',
+            b: 'Rússia',
+            c: 'Espanha',
+            d: 'África do Sul'
+        },
+        respostaCerta: 2
+    },
+    {
+        pergunta: '“Arábica” e “robusta” são tipos de quê?',
+        respostas:{ 
+            a: 'Uva',
+            b: 'Café',
+            c: 'Melão',
+            d: 'Laranja'
+        },
+        respostaCerta: 2
+    },
+    {
+        pergunta: 'Que símbolo está desenhado no centro da bandeira Argentina?',
+        respostas:{ 
+            a: 'Sol',
+            b: 'Lua',
+            c: 'Estrela',
+            d: 'Foice e Martelo'
+        },
+        respostaCerta: 1
+    },
+    {
+        pergunta: 'O melhor time de Dev se encontra em qual empresa?',
+        respostas:{ 
+            a: 'Microsoft',
+            b: 'Google',
+            c: 'Amazon',
+            d: 'Jovens Gênios'
+        },
+        respostaCerta: 4
+    }
+]
--- script.js
+++ script.js
@@ -1,23 +1,125 @@
-var respostasCertas = ['1', '3', '4', '2']
-var respostasUsuario = []
-function respondeu1(){
-    respostasUsuario.push('1')
-   
-}
 
-function respondeu2(){
-    respostasUsuario.push('2')
+var perguntas = document.getElementById('perguntas')
+var opt1 = document.getElementById('opt1')
+var opt2 = document.getElementById('opt2')
+var opt3 = document.getElementById('opt3')
+var opt4 = document.getElementById('opt4')
+var res = document.getElementById('res')
+var resImg = document.getElementById('resImg')
+var resImg2 = document.getElementById('resImg2')
+var resImgMilhao = document.getElementById('resImgMilhao')
+var resImg4 = document.getElementById('perdeuTudo')
+var errar = document.getElementById('errarP')
+var parar = document.getElementById('pararP')
+var acertar = document.getElementById('acertarP')
 
-}
+var divStyle = document.getElementsByClassName('divs')
 
-function respondeu3(){
-    respostasUsuario.push('3')
+var numQuestaoGlobal = 0
+var respostaUsuario
+var numPergunta = 0
 
-}
-
-function respondeu4(){
-    respostasUsuario.push('4')
+function mostrarQuestao(){
     
 
+    res.innerHTML =''
+    res.style.color = ''
+    resImg.src = ''
+    resImg2.src = ''
+    numQuestao = Math.floor(Math.random()*questoes.length)
+    
+    if(numPergunta == 10){
+        numQuestao = 19
+        numQuestaoGlobal = numQuestao
+    }else{
+        numQuestaoGlobal = numQuestao
+    }
+    console.log(numQuestaoGlobal)
+    perguntas.innerHTML = questoes[numQuestaoGlobal].pergunta
+    opt1.innerHTML = questoes[numQuestaoGlobal].respostas.a
+    opt2.innerHTML = questoes[numQuestaoGlobal].respostas.b
+    opt3.innerHTML = questoes[numQuestaoGlobal].respostas.c
+    opt4.innerHTML = questoes[numQuestaoGlobal].respostas.d
+    
+    if(numPergunta == 0){
+        errar.innerHTML = 0
+        parar.innerHTML = 0
+        acertar.innerHTML = `${1} MIL`
+    
+    }else if(numPergunta > 0 && numPergunta <= 4){
+        acertar.innerHTML = `${numPergunta*20} MIL`
+        parar.innerHTML = `${numPergunta*12} MIL`
+        errar.innerHTML = `${numPergunta*5} MIL`
+    }else if(numPergunta > 4 && numPergunta <= 9){
+        acertar.innerHTML = `${numPergunta*50} MIL`
+        parar.innerHTML = `${numPergunta*25} MIL`
+        errar.innerHTML = `${numPergunta*10} MIL`
+    } else{
+        acertar.innerHTML = `MILHÃO`
+        parar.innerHTML = `${numPergunta*50} MIL`
+        errar.innerHTML = 0
+        perguntaMilhao.src = 'audio/pergunta-final.mp3'
+        
+    }
+    if(numPergunta == 2){
+        perguntaUm.src = 'audio/pergunta-40mil.mp3'
+    } else if(numPergunta == 6){
+        perguntaTres.src = 'audio/pergunta-300mil.mp3'
+    } else if(numPergunta == 8){
+        perguntaDois.src = 'audio/pergunta-400mil.mp3'
+    }
+
+    numPergunta ++
+    formatar()
 }
+
+function respondeu(resposta){
+    respostaUsuario = parseInt(resposta.id)
+
+    if(numPergunta == 11 && respostaUsuario == questoes[numQuestaoGlobal].respostaCerta){
+        resImg3.src = 'img/silvio-premio.png'
+        resImgMilhao.src = 'img/milhao.png'
+        res.innerHTML = 'PARABÉNS !!!<br>VOCÊ GANHOU<br>UM MILHÃO !!!'
+        voceGanhou.src = 'audio/umMilhao.mp3'
+        
+    }else if(numPergunta == 11 && respostaUsuario != questoes[numQuestaoGlobal].respostaCerta){
+        res.innerHTML = 'PERDEU TUDO !!!'
+        res.style.color = 'red'
+        resImg4.src = '/img/silvio-santos-decepcionado.png'
+        errou.src = 'audio/voce-errou.mp3'
+    }else if(respostaUsuario == questoes[numQuestaoGlobal].respostaCerta){
+        resposta.classList.replace('respostasstyle', 'respostaCorreta')
+        res.innerHTML ='Você ACERTOU !'
+        res.style.color = 'green'
+        resImg2.src = '/img/silvio-coracao.png'
+        voceAcertou.src = '/audio/certa-resposta.mp3'
+
+    }else {
+        res.innerHTML ='Você PERDEU !'
+        res.style.color = 'red'
+        resImg.src = '/img/silvio-cara-seria.png'
+        errou.src = 'audio/voce-errou.mp3'
+    }
+    questoes.splice(numQuestaoGlobal, 1)
+}
+
+function formatar(){
+    for(let i = 0; i < divStyle.length; i++){
+        divStyle[i].classList.replace('respostaCorreta','respostasstyle')
+    }
+}
+
+    
+
+
+
+    
+   
+
+
+
+
+
+
+
 
